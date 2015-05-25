2ae82fdeb833ac2467b7591a6317006da8da7b0b
Matthew Powers <matthewkevinpowers@gmail.com>
2015-05-25 13:33:44 -0400

    Add a missing require

--- lib/slack_trello.rb
+++ lib/slack_trello.rb
@@ -1,6 +1,7 @@
 require "slack_trello/version"
 require "slack_trello/response_parser"
 require "slack_trello/text_parser"
+require "slack_trello/work_command"
 
 module SlackTrello
   # Your code goes here...
