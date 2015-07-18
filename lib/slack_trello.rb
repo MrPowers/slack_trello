bdaec35b2d30c93dd5ad963a56502455c56a37a8
Matthew Powers <matthewkevinpowers@gmail.com>
2015-07-18 18:25:49 -0400

    Add a generic class to create Trello cards

--- lib/slack_trello.rb
+++ lib/slack_trello.rb
@@ -1,12 +1,13 @@
-require "pry"
-
 require "slack-notifier"
 
 require "slack_trello/version"
 require "slack_trello/text_parser"
 require "slack_trello/response_parser"
-require "slack_trello/work_command"
+
+require "slack_trello/create_trello_card"
 require "slack_trello/speaker"
+
+require "slack_trello/work_command"
 require "slack_trello/retro_command"
 
 module SlackTrello
---
+++ lib/slack_trello/create_trello_card.rb
@@ -0,0 +1,39 @@
+module SlackTrello; class CreateTrelloCard
+
+  attr_reader :board_name, :list_name, :card_name
+
+  def initialize(args)
+    @board_name = args.fetch(:board_name)
+    @list_name = args.fetch(:list_name)
+    @card_name = args.fetch(:card_name)
+  end
+
+  def card
+    return @card if @card
+    card = Trello::Card.new
+    card.name = card_name
+    card.list_id = trello_list.id
+    card.save
+    @card = card
+  end
+
+  private
+
+  def trello_board
+    @trello_board ||= Trello::Board.all.find do |b|
+      spaceify(b.name) == spaceify(board_name)
+    end
+  end
+
+  def trello_list
+    @trello_list ||= trello_board.lists.find do |l|
+      spaceify(l.name) == spaceify(list_name)
+    end
+  end
+
+  def spaceify(str)
+    str.gsub(/-|_/, " ").downcase
+  end
+
+end; end
+
