876504298c9966e472c4b17b1385cc0939cc5bdc
chickenriceplatter <chickenriceplatter@gmail.com>
2015-10-21 15:03:43 -0400

    fix nonexistent method call on nil objects

--- lib/slack_trello/trello_helpers/lookup.rb
+++ lib/slack_trello/trello_helpers/lookup.rb
@@ -10,6 +10,7 @@ module SlackTrello; module TrelloHelpers; class Lookup
 
     def list(board_name, list_name)
       b = board(board_name)
+      return nil unless b
       b.lists.find do |l|
         spaceify(l.name) == spaceify(list_name)
       end
@@ -17,6 +18,7 @@ module SlackTrello; module TrelloHelpers; class Lookup
 
     def card(board_name, list_name, card_name)
       l = list(board_name, list_name)
+      return nil unless l
       l.cards.find do |c|
         spaceify(c.name) == spaceify(card_name)
       end
